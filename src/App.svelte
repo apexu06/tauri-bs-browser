335648648b476d544c2ceb18f768e46f4c488139
apexu <jakob.zelger@outlook.at>
2023-06-13 16:20:11 +0200

    basically inital commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Greet from './lib/Greet.svelte';
+	import './styles.css';
+</script>
+
+<main class="container">
+	<h1>Welcome to Tauri!</h1>
+	<div class="bg-red-400">fuck no</div>
+
+	<Greet />
+</main>
+
+<style>
+</style>
